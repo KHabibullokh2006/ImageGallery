710f0df5557ad87db2ed875082606ac59fb109da
PC902 <khabibullokh2006@gmail.com>
2023-02-11 11:13:25 +0500

    Image Gallery

---
+++ app/src/main/java/uz/itschool/myapplication/MainActivity.kt
@@ -0,0 +1,59 @@
+package uz.itschool.myapplication
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var images = mutableListOf<ImageView>()
+    var index = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        images.add(img1)
+        images.add(img2)
+        images.add(img3)
+        images.add(img4)
+        images.add(img5)
+        images.add(img6)
+        images.add(img7)
+        images.add(img8)
+        images.add(img9)
+
+        img1.setOnClickListener(this)
+        img2.setOnClickListener(this)
+        img3.setOnClickListener(this)
+        img4.setOnClickListener(this)
+        img5.setOnClickListener(this)
+        img6.setOnClickListener(this)
+        img7.setOnClickListener(this)
+        img8.setOnClickListener(this)
+        img9.setOnClickListener(this)
+
+        next.setOnClickListener {
+            if (index < images.size - 1) {
+                index++
+            } else {
+                index = 0
+            }
+
+        }
+
+    }
+
+    @SuppressLint("ResourceType")
+    override fun onClick(p0: View?) {
+        var img = findViewById<ImageView>(p0!!.id)
+        image_layout.visibility = View.GONE
+        action_layout.visibility = View.VISIBLE
+        img.setImageResource(img.id)
+
+    }
+}
